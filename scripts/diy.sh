39922e9c7136b46c51dda1c4e3664f36f6b80631
topstarxxx <topstarxxx@gmail.com>
2025-03-02 12:18:46 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='ImmortalWrt'/g" package/base-files/files/bin/config_generate
 #更改默认源地址为上海交大源
 sed -i "s,mirrors.vsean.net/openwrt,mirrors.sdu.edu.cn/immortalwrt,g" package/emortal/default-settings/files/99-default-settings-chinese
 
