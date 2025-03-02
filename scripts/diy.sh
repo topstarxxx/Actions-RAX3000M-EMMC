2defdf7b61085e34ef48239061187c5dd6f4d249
topstarxxx <topstarxxx@gmail.com>
2025-03-02 12:25:34 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,10 +2,24 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='ImmortalWrt'/g" package/base-files/files/bin/config_generate
-#更改默认源地址为上海交大源
+
+#更改默认源地址为山大源
 sed -i "s,mirrors.vsean.net/openwrt,mirrors.sdu.edu.cn/immortalwrt,g" package/emortal/default-settings/files/99-default-settings-chinese
 
+##更改主机名
+sed -i "s/hostname='.*'/hostname='ImmortalWrt'/g" package/base-files/files/bin/config_generate
+
+##加入作者信息
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='ImmortalWrt-$(date +%Y%m%d)'/g"  package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By Toopstarxxx'/g" package/base-files/files/etc/openwrt_release
+
+##WiFi
+sed -i "s/MT7981_AX3000_2.4G/CMCC_dd-wrt/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i "s/MT7981_AX3000_5G/CMCC_dd-wrt_5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
+
+##New WiFi
+sed -i "s/ImmortalWrt-2.4G/CMCC_dd-wrt/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/CMCC_dd-wrt_5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 #添加kmod-ipt-socket等Passwall必须模块
 cp -rf $GITHUB_WORKSPACE/patchs/iptables-makefile.patch $GITHUB_WORKSPACE/openwrt/p1.patch
 patch -p1 < $GITHUB_WORKSPACE/openwrt/p1.patch
